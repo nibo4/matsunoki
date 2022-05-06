2f84c288f5e9c8202a5e1a7a047826df1d8c8fb1
himanoa <matsunoappy@gmail.com>
2022-05-06 14:08:33 +0900

    Fix migration sql syntax

--- database/auth/migrate.sql
+++ database/auth/migrate.sql
@@ -1,10 +1,10 @@
-create table login_providers {
-  user_id: text not null,
-  kind: varchar(255) not null,
-  id_in_provider: varchar(255) not null
-};
+create table login_providers (
+  user_id text not null,
+  kind varchar(255) not null,
+  id_in_provider varchar(255) not null
+);
 
-create table users {
-  id: text not null,
-};
+create table users (
+  id text not null
+);
 
