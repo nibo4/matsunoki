90c1842d3d37fe0add64211d5592c9d34ff31f8b
himanoa <matsunoappy@gmail.com>
2022-05-06 14:22:17 +0900

    Add primary key

--- database/auth/migrate.sql
+++ database/auth/migrate.sql
@@ -1,7 +1,8 @@
 create table login_providers (
   user_id text not null,
   kind varchar(255) not null,
-  id_in_provider varchar(255) not null
+  id_in_provider varchar(255) not null,
+  PRIMARY KEY (kind, id_in_provider, user_id)
 );
 
 create table users (
