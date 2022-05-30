b9beb17901e4f07c35c6039dd520fd6634b0ebb0
himanoa <matsunoappy@gmail.com>
2022-05-31 01:11:49 +0900

    Change ui port

--- web/ui/src/index.tsx
+++ web/ui/src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import '@picocss/pico/pico.classless.min.css'
+import '@picocss/pico/css/pico.classless.min.css'
 import './index.css';
 import App from './App';
 
--- web/ui/vite.config.ts
+++ web/ui/vite.config.ts
@@ -3,6 +3,9 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  server: {
+    port: 3001
+  },
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
