a75fb708062dc4ba6d96dfc6efb38a86f3738f73
himanoa <matsunoappy@gmail.com>
2022-06-02 02:10:09 +0900

    Add eslint-config package

---
+++ web/eslint-config/index.js
@@ -0,0 +1,62 @@
+module.exports = {
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "sourceType": "module",
+    "project": "./tsconfig.json" 
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: "^_.+$", argsIgnorePattern: "^_.+$" }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'property',
+        format: null,
+        filter: '^(_|__html)$',
+      },
+      {
+        selector: 'parameter',
+        format: null,
+        filter: '^_',
+      },
+      {
+        selector: 'variable',
+        format: null,
+        filter: '^_',
+      },
+      {
+        selector: 'variableLike',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': 'error',
+    "@typescript-eslint/explicit-function-return-type": 'error',
+    'no-unused-vars': 'off',
+    'import/order': 'error',
+    'eqeqeq': ['error', 'smart'],
+  }
+}
