b40ab149a1200aaa6538ac204ec8f38262fe7708
himanoa <matsunoappy@gmail.com>
2022-06-04 03:15:40 +0900

    Add sign-up test

---
+++ web/api-client/src/endpoints/sign-up.test.ts
@@ -0,0 +1,16 @@
+import { responseHandler } from './sign-up'
+
+describe("responseHandler", () => {
+  it("error", () => {
+    expect(responseHandler({ dummy: 123 }).err).toStrictEqual(true)
+  })
+
+  it("ok", () => {
+    const actual = responseHandler({ user_id: 'xxx', name: 'yyyy' })
+    expect(actual.ok).toStrictEqual(true)
+    expect(actual.val).toStrictEqual({
+      userId: 'xxx',
+      name: 'yyyy'
+    })
+  })
+})
