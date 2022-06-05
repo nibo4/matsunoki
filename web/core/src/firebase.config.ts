b8c9cb75d53b4d2ce311e1a6f028d7e00cfbeea8
himanoa <matsunoappy@gmail.com>
2022-06-06 06:12:16 +0900

    Add firebase config in .gitignore

--- web/core/src/firebase.config.ts
+++ web/core/src/firebase.config.ts
@@ -1,2 +1,9 @@
 // Set your firebase configuration
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "AIzaSyA1p3r8Us0aJROzc2mxzuwQTGmyg-pd1Ns",
+  authDomain: "extreme-axon-283103.firebaseapp.com",
+  projectId: "extreme-axon-283103",
+  storageBucket: "extreme-axon-283103.appspot.com",
+  messagingSenderId: "399667618661",
+  appId: "1:399667618661:web:65b579ebf5edc72387378c",
+};
