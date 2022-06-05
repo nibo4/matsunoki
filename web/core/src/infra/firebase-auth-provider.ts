52260a2bcd226b965b3891def61b9f809889dbeb
himanoa <matsunoappy@gmail.com>
2022-06-06 06:21:29 +0900

    Implemented firebase-auth-provider infra

--- web/core/src/infra/firebase-auth-provider.ts
+++ web/core/src/infra/firebase-auth-provider.ts
@@ -1,5 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import type { SignInWithProvider } from "../interface/";
+import { firebaseConfig } from "../firebase.config";
 
-export const firebaseSignInWithProvider: SignInWithProvider = (_provider) => {
-  return Promise.resolve("foo");
+export const firebaseSignInWithProvider: SignInWithProvider = async (
+  _provider
+) => {
+  initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const signInResult = await signInWithPopup(auth, provider);
+  const token = GoogleAuthProvider.credentialFromResult(signInResult);
+  if (token == null || token.idToken == null) {
+    throw new Error("token is required(actual: null)");
+  }
+  return token.idToken;
 };
--- web/core/src/interface/auth-provider.ts
+++ web/core/src/interface/auth-provider.ts
@@ -1,4 +1,4 @@
 export type Provider = "google";
-export type Token = string;
+export type AccessToken = string;
 
-export type SignInWithProvider = (p: Provider) => Promise<Token>;
+export type SignInWithProvider = (p: Provider) => Promise<AccessToken>;
