8b38ab7dab51e1a6eae48e96c5b4624bffe2d183
himanoa <matsunoappy@gmail.com>
2022-06-07 21:40:08 +0900

    Use environment variables in firebase config

--- web/core/src/env.d.ts
+++ web/core/src/env.d.ts
@@ -1,6 +1,13 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly ACCOUNT_API_HOST: string;
+
+  readonly FIREBASE_API_KEY: string;
+  readonly FIREBASE_AUTH_DOMAIN: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_STORAGE_BUCKET: string;
+  readonly FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly FIREBASE_MESSAGING_APP_ID: string;
 }
 
 interface ImportMeta {
