d7c51f7b5e7c60e46901ff6764626e5dbabbf33a
himanoa <matsunoappy@gmail.com>
2022-06-07 21:36:50 +0900

    Fix firebase config

--- web/core/src/firebase.config.ts
+++ web/core/src/firebase.config.ts
@@ -1,9 +1,2 @@
 // Set your firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyA1p3r8Us0aJROzc2mxzuwQTGmyg-pd1Ns",
-  authDomain: "extreme-axon-283103.firebaseapp.com",
-  projectId: "extreme-axon-283103",
-  storageBucket: "extreme-axon-283103.appspot.com",
-  messagingSenderId: "399667618661",
-  appId: "1:399667618661:web:65b579ebf5edc72387378c",
-};
+export const firebaseConfig = {};
