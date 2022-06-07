0aefceb6d28d2e225afc4352410b205495762879
himanoa <matsunoappy@gmail.com>
2022-06-08 00:22:26 +0900

    Remove vite-plugin-ssr

--- web/ssr-server/vite.config.ts
+++ web/ssr-server/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import svgPlugin from 'vite-plugin-solid-svg'
-import ssrPlugin from 'vite-plugin-ssr/plugin'
 
 export default defineConfig({
-  plugins: [solidPlugin(), svgPlugin(), ssrPlugin()],
+  plugins: [solidPlugin(), svgPlugin()],
   server: {
     port: 3001
   },
